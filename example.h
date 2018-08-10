ae7a32a153ef89f87bd7d1f594c55e33823c2e31
yongping.pi <yongping.chen@kii.com>
2018-08-10 06:36:30 +0000

    change to new app

--- example.h
+++ example.h
@@ -6,13 +6,13 @@ extern 'C' {
 #endif
 
 /* Go to https:/developer.kii.com and create app for you! */
-const char EX_APP_ID[] = "ex4liedteote";
-const char EX_APP_KEY[] = "d787de623f15483fa887815017f010e6";
+const char EX_APP_ID[] = "rr7oqyvzaonp";
+const char EX_APP_KEY[] = "e1def1f14e194a4d9d23911a2e8d6fa5";
 /* JP: "api-jp.kii.com" */
 /* US: "api.kii.com" */
 /* SG: "api-sg.kii.com" */
 /* CN: "api-cn3.kii.com" */
-const char EX_APP_SITE[] = "api.kii.com";
+const char EX_APP_SITE[] = "api-jp.kii.com";
 
 #define EX_COMMAND_HANDLER_BUFF_SIZE 4096
 #define EX_STATE_UPDATER_BUFF_SIZE 4096
